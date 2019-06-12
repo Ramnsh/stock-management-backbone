fc7eb6ea1832084de54da2020596a66050bf041f
Ramneek Sharma <ramneeksharma@Ramneek.local>
2019-06-12 10:28:00 +0530

    feat(database connection): "Added database connection"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./config/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Connect to MySQL on start
+
+db.connect(db.MODE_PRODUCTION, function(err) {
+    if (err) {
+      console.log('Unable to connect to MySQL.')
+      process.exit(1)
+    } else {
+      app.listen(3002, function() {
+        console.log('DB listening on port 3002...')
+      })
+    }
+  })
 
 module.exports = app;
