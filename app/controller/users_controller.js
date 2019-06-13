7a8a17298ddaa3651c8f833fcb90a06be082ed8d
Anand <anand1@MacBook-Pro.local>
2019-06-13 18:37:34 +0530

    feat(user_model): "Add basic user model" - add user model - add create user api

--- app/controller/users_controller.js
+++ app/controller/users_controller.js
@@ -1,3 +1,15 @@
+var User = require('../model/user_model.js');
+
 exports.signIn = function(req,res) {
     res.send('hello');
+}
+
+exports.createUser = function(req, res) {
+    let newUser = new User(req.body);
+    User.createUser(newUser, (err, user)=> {
+        if(err) {
+            res.send(err);
+        }
+        res.json(user);
+    })
 }
